9fcb17a7ad2dfc6bc83aa70a69d2dbd74ca69b16
sikander-afzaal <sikander.aaa@gmail.com>
2023-03-27 15:36:50 +0500

    update

--- src/components/HurrayModal.jsx
+++ src/components/HurrayModal.jsx
@@ -1,11 +1,27 @@
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
+
 const HurrayModal = ({ setModal }) => {
+  const { width, height } = useWindowSize();
   return (
     <>
       <div
         onClick={() => setModal(false)}
+        className="fixed sm:opacity-100 opacity-70 top-0 left-0 z-[93]"
+      >
+        <Confetti
+          width={width}
+          height={height}
+          gravity={0.1}
+          numberOfPieces={200}
+        />
+      </div>
+      <div className="sm:hidden block bg-[#f1deeb] fixed top-0 left-0 w-full h-full z-[92]"></div>
+      <div
+        onClick={() => setModal(false)}
         className="fixed top-0 right-0 bg-black bg-opacity-60 z-[91] h-full w-full cursor-pointer"
       ></div>
-      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-5 pt-24 pb-6 w-full sm:max-w-[450px] flex justify-start max-h-full sm:max-h-[90%] overflow-y-auto bg-[#F1DEEB] z-[100] h-full sm:h-auto sm:rounded-2xl items-center overflow-hidden flex-col gap-2">
+      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-5 pt-24 pb-6 w-full sm:max-w-[450px] flex justify-start max-h-full sm:max-h-[90%] overflow-y-auto sm:bg-[#F1DEEB] z-[100] h-full sm:h-auto sm:rounded-2xl items-center overflow-hidden flex-col gap-2">
         {/* <img
           src="/blur.png"
           className="absolute top-0 left-0 z-20 -translate-x-1/2 -translate-y-1/2 blur-[235px]"
